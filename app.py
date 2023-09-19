c52ca6f488042aa6c52f76c05bbd16de8402d432
francescoBianchi7 <81578895+francescoBianchi7@users.noreply.github.com>
2023-09-19 14:55:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from views import app
-import os
+
 
 if __name__ == '__main__':
-    host = os.getenv('FLASK_HOST', '127.0.0.1')
-    port = os.getenv('FLASK_PORT', '5000')
-    app.run(host=host, port=int(port))
+    app.run()
