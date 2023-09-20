de567c764a8d8308698e6b2017b8337d0e83d438
francescoBianchi7 <81578895+francescoBianchi7@users.noreply.github.com>
2023-09-20 09:54:17 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,6 @@ export class Config {
     static BASE_URL = `${this.SERVER_URL}`;
 
 }
-const SCRIPT_ROOT = {{ request.script_root|tojson }}
-console.log('SCRIPT_ROOT',SCRIPT_ROOT)
+
 console.log('SERVER_URL=',Config.SERVER_URL)
 console.log('BASE_URL=',Config.BASE_URL)
